b1a8b63e0c68569c50eb6237fc40e169b7f73e1c
RemiZOffAlex <remizoffalex@gmail.com>
2015-08-12 02:44:54 +0300

    Хеш пароля SHA-512

---
+++ Python/get_hash_password.py
@@ -0,0 +1,20 @@
+##!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import base64
+import uuid
+import hashlib
+
+def get_hash_password(password, salt = None):
+    """
+    Получить хеш пароля SHA-512
+    """
+    if salt == None:
+        salt = uuid.uuid4().hex
+    text = password.encode('utf-8') + salt.encode('utf-8')
+    h = hashlib.sha512()
+    h.update(text)
+    return str(h.hexdigest())
+
+print(get_hash_password('password'))
+print(get_hash_password('password', 'salt'))
