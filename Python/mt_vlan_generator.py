27b9035b0ff496997fdc102fad3ef2988bb41cde
alex-eri <oddghost@mail.ru>
2016-04-11 23:06:22 +0300

    Update mt_vlan_generator.py

--- Python/mt_vlan_generator.py
+++ Python/mt_vlan_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-__author__ = 'RemiZOffAlex'
+__author__ = 'RemiZOffAlex, Eri'
 __copyright__ = '(c) RemiZOffAlex'
 __license__ = 'MIT'
 __email__ = 'remizoffalex@mail.ru'
@@ -10,18 +10,16 @@ __email__ = 'remizoffalex@mail.ru'
 Генератор команд для MikroTik
 Для тех, у кого много VLAN
 
-Пример применения шаблонов
+Пример применения шаблонов, распаковка звездочками
 '''
 
 import os
 import sys
 
-from jinja2 import Template
-
-templates = """/interface vlan
-add interface=bridge-local name=vlan{{ item['id'] }} vlan-id={{ item['id'] }}
+template = """/interface vlan
+add interface=bridge-local name=vlan{id} vlan-id={id}
 /ip address
-add interface=vlan70 address={{ item['ip'] }}/24
+add interface=vlan{id} address={ip}/24
 
 """
 
@@ -29,6 +27,5 @@ vlans = [{'id': 70, 'ip': '192.168.0.2'},
 {'id': 80, 'ip': '192.168.0.3'}]
 
 for item in vlans:
-    template=Template(templates)
-    template=template.render(item=item)
-    print(template)
+    conf=template.format(**item)
+    print(conf)
