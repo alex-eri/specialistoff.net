d13be699c70bc6c7bf815fff51c64293e7a6ad9f
alex-eri <oddghost@mail.ru>
2016-04-11 21:29:22 +0300

    simply clever

--- Python/net_generator.py
+++ Python/net_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-__author__ = 'RemiZOffAlex'
+__author__ = 'RemiZOffAlex, eri'
 __copyright__ = '(c) RemiZOffAlex'
 __license__ = 'MIT'
 __email__ = 'remizoffalex@mail.ru'
@@ -9,27 +9,23 @@ __email__ = 'remizoffalex@mail.ru'
 '''
 Генератор конфигурационного файла для сетевых интерфейсов
 
-Пример применения шаблонов
+Пример применения нативных шаблонов
 '''
 
 import os
 import sys
 
-from jinja2 import Template
-
-templates = {"debian": """auto eth0:{{ i }}
-iface eth0:{{ i }} inet static
-	address {{ ip }}
+templates = {"debian": """auto eth0:{i}
+iface eth0:{i} inet static
+	address {ip}
 	netmask 255.255.255.0
 
 """,
-"freebsd": '''ifconfig_re0="inet {{ ip }} netmask 255.255.255.0 up"'''}
+"freebsd": '''ifconfig_re0="inet {ip} netmask 255.255.255.0 up"'''}
 
-listip = """192.168.0.2
-192.168.0.3
-"""
+listip = "192.168.0.2 192.168.0.3"
 
-for i, line in enumerate(listip.split()):
-    template=Template(templates["debian"])
-    template=template.render(i=i, ip=line)
+for i, line in enumerate(listip.strip().split()):
+    template=templates["debian"]
+    template=template.format(i=i, ip=line)
     print(template)
