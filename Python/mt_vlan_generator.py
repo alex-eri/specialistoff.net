b82affd9b5d33fa3d7543eb15cacf68bbafa3d6d
RemiZOffAlex <remizoffalex@gmail.com>
2016-04-11 14:12:27 +0300

    MikroTik генератор команд для большого количества VLAN

---
+++ Python/mt_vlan_generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+__author__ = 'RemiZOffAlex'
+__copyright__ = '(c) RemiZOffAlex'
+__license__ = 'MIT'
+__email__ = 'remizoffalex@mail.ru'
+
+'''
+Генератор конфигурационного файла для сетевых интерфейсов
+
+Пример применения шаблонов
+'''
+
+import os
+import sys
+
+from jinja2 import Template
+
+templates = """/interface vlan
+add interface=bridge-local name=vlan{{ item['id'] }} vlan-id={{ item['id'] }}
+/ip address
+add interface=vlan70 address={{ item['ip'] }}/24
+
+"""
+
+vlans = [{'id': 70, 'ip': '192.168.0.2'},
+{'id': 80, 'ip': '192.168.0.3'}]
+
+for item in vlans:
+    template=Template(templates)
+    template=template.render(item=item)
+    print(template)
